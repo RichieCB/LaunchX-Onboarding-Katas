e6b4bcb5ea35ab28baf4c3c72edfaf2408283121
Ricardo Caballero <ricardo.caballero@alumno.buap.mx>
2022-02-14 22:00:24 -0600

    Add files via upload

---
+++ kata1/intro.py
@@ -0,0 +1,27 @@
+
+sum = 1+2
+print(sum)
+
+#--------------- Impirmir en pantalla---------------
+
+print(" Hola desde consola")
+
+#--------------- Fechas ------------------------------
+from datetime import date
+
+date.today()
+print(date.today())
+
+#--------- Conversion de tipos de datos----------------
+
+print("Today's date is " +str(date.today()))
+
+#--------------- Entrada desde teclado-------------
+name = input("Ingresa tu nombre \n")
+
+#------------- Leer numeros de teclado-------
+
+edad = int(input("Ingrese su edad: \n")) #Se tiene que enmascarar como int
+
+
+
